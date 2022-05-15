81256196487fe98711db4fc4b1fc5652d981d7e8
Nunes <benedito.nunes@atos.net>
2022-05-15 14:48:51 -0300

    End point testado

--- src/main/java/com/logos/cursomc/CursomcApplication.java
+++ src/main/java/com/logos/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.logos.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +10,4 @@ public class CursomcApplication {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-}
+}	
